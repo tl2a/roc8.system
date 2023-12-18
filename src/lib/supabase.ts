ce6083fea30f8267d057c7c733e24a3583259a52
Arpan Biswas <talk2arpan@protonmail.com>
2023-12-18 21:19:33 +0530

    fix: removing more type mistakes with restrict rules v2

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient('https://dzetcmoqhnkfyaaghnly.supabase.co', process.env.SUPABASE_TOKEN);
+const supabase = createClient('https://dzetcmoqhnkfyaaghnly.supabase.co', process.env.SUPABASE_TOKEN as string);
 
 export default supabase;
